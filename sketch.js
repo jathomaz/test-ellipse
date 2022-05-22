3e6a60136a35c67352d7b5afb0f915bfcb518c4c
Jackie Thomas <jackiethomaz@gmail.com>
2022-05-22 11:03:09 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let hueX=0;
+function setup() {
+  createCanvas(600, 400);
+  colorMode(HSB);
+  frameRate(1);
+   noLoop();
+   background(220); 
+}
+
+function draw() {
+  
+  for (let i= 0;i<20;i++){
+    fill(130,100,100);
+    ellipse(i*8,20,20,20);
+    hueX+=10;
+  }
+  for (let j= 0;j<20;j++){
+    fill(hueX,100,100);
+    ellipse(j*8,80,30,30);
+    hueX+=10;
+  }
+  for (let j= 0;j<10;j++){
+    fill(hueX,100,100);
+    ellipse(j*8,180,30,30);
+    hueX+=10;
+  }
+}
